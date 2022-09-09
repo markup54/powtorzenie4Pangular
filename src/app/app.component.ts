e414e1dd2f3e47cf480586c430bc105e680600dc
markup54 <marzenakupka@tlen.pl>
2022-09-09 10:51:12 +0200

    do ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'powtorzenie4P';
+  licznik :number =0;
+  urlZdj1:string = "assets/images/zdj1.jpg";
+  urlZdj2:string = "assets/images/zdj2.jpg";
+  urlZdj3:string = "assets/images/zdj3.jpg";
+  kursy:string[] = [
+    "Programowanie w JS",
+    "Algorytmy",
+    "Zarządzanie projektami",
+    "Progromowanie SPA w Angular"
+  ];
+  czyAktywne:boolean = false;
+  zwieksz():void{
+    this.licznik++;
+  }
 }
